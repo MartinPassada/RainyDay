82777dde891edb42648eba504d8905a4613355a2
Martin <Martin_passada@outlook.com>
2020-07-10 19:10:05 -0300

    fixes

--- Server/server.js
+++ Server/server.js
@@ -24,6 +24,7 @@ app.use(session({
 
 // Recursos estaticos
 app.use(express.static(path.join(__dirname, '../Client')));
+app.use(express.static(path.join(__dirname, '../Client/img')));
 
 // Configuracion Handlebars
 app.engine('handlebars', exphbs({
